7dc175d895d11efe3578a5f4cf74acf09e46522a
Jason <kangja1996@gmail.com>
2021-02-19 01:26:07 -0500

    progress

--- practical-react/src/components/FetchRandomUser.js
+++ practical-react/src/components/FetchRandomUser.js
@@ -10,13 +10,22 @@ export default class FetchRandomUser extends React.Component {
     const url = "https://api.randomuser.me/";
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ person: data.results[0] });
-    console.log(data.results[0]);
+    this.setState({ person: data.results[0], loading: false });
   }
 
   render() {
     return (
-      <div>{this.state.loading ? <div>loading</div> : <div>person..</div>}</div>
+      <div>
+        {this.state.loading || !this.state.person ? (
+          <div>loading</div>
+        ) : (
+          <div>
+            <div>{this.state.person.name.title}</div>
+            <div>{this.state.person.name.first}</div>
+            <div>{this.state.person.name.last}</div>
+          </div>
+        )}
+      </div>
     );
   }
 }
